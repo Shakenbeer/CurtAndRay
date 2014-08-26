946a166ea8fe2ffe2979be85827d613ba7bbe56e
shakenbeer <shakenbeer@gmail.com>
2014-08-26 21:11:27 +0300

    assets holder in form of banch of static fields

---
+++ src/com/shakenbeer/curtandray/assets/Assets.java
@@ -0,0 +1,40 @@
+package com.shakenbeer.curtandray.assets;
+
+import com.badlogic.androidgames.framework.Pixmap;
+import com.badlogic.androidgames.framework.Sound;
+
+public class Assets {
+    public static Pixmap arrowLeft;
+    public static Pixmap arrowRight;
+    public static Pixmap curt;
+    public static Pixmap digits;
+    public static Pixmap flag;
+    public static Pixmap higscores;
+    public static Pixmap howTo;
+    public static Pixmap howToScreen1Lossy;
+    public static Pixmap howToScreen1;
+    public static Pixmap howToScreen2Lossy;
+    public static Pixmap howToScreen2;
+    public static Pixmap levelLossy;
+    public static Pixmap level;
+    public static Pixmap logoLossy;
+    public static Pixmap logo;
+    public static Pixmap mine;
+    public static Pixmap nextLevel;
+    public static Pixmap pauseLossy;
+    public static Pixmap pause;
+    public static Pixmap play;
+    public static Pixmap ray;
+    public static Pixmap screenLossy;
+    public static Pixmap screen;
+    public static Pixmap soundOn;
+    public static Pixmap soundOff;
+    public static Pixmap start;
+
+    public static Sound click;
+    public static Sound hideMine;
+    public static Sound setupFlag;
+    public static Sound setupMine;
+    public static Sound win;
+
+}
